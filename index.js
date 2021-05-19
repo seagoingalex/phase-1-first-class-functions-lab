57c74fea0758bd8a14da9663a00d7a9886c2bf7c
Alex Calvert <alexanderajcalvert@gmail.com>
2021-05-19 10:34:08 -0700

    commit tree baecca31c7a74d007ae775f5bf87af66ca629ed6

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driverArray) {
+    return driverArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(driverArray) {
+    return driverArray.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+createFareMultiplier = function(multiplyValue) {
+    return endValue => multiplyValue*endValue;
+}
+
+/*
+const fareDoubler = ((multiplybyTwo) => multiplybyTwo*2);
+const fareTripler = ((multiplybyThree) => multiplybyThree*3);
+
+function fareDoubler(multiplybyTwo) {
+    return multiplybyTwo*2;
+};
+
+function fareDoubler(multiplybyTwo) {
+    return createFareMultiplier(multiplybyTwo);
+};
+
+function fareTripler(multiplybyThree) {
+    return multiplybyThree*3;
+};
+
+const fareDoubler=((fare)=>createFareMultiplier(2)(fare));
+const fareTripler=((fare)=>createFareMultiplier(3)(fare));
+
+*/
+
+function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers, driverReturnFunction) {
+    return driverReturnFunction(drivers);
+}
